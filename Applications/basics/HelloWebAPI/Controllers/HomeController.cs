f5683e0fdf7bc4f56889cfbc7ef3f22586becaaf
atillakalay <atilla.kalayy@gmail.com>
2023-03-25 16:00:16 +0300

    The 2nd part courses have been completed.

---
+++ Applications/basics/HelloWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using HelloWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWebAPI.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            var result = new ResponseModel { HttpStatusCode = 404, Message = "Aradağınız sayfa bulunamadı!" };
+            return Ok(result);
+        }
+
+    }
+}
---
+++ Applications/basics/HelloWebAPI/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloWebAPI.Models
+{
+    public class ResponseModel
+    {
+        public int HttpStatusCode { get; set; }
+        public string? Message { get; set; }
+    }
+}
