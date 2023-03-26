9528d71fc726d28fc29cc123190c92476748c5d5
atillakalay <atilla.kalayy@gmail.com>
2023-03-26 19:26:47 +0300

    Chapter 3 courses have been completed.

---
+++ Applications/basics/ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProduct()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1,ProductName = "Computer"},
+                new Product { Id = 2,ProductName = "Keyboard"},
+                new Product { Id = 3,ProductName = "Mouse"},
+            };
+
+            _logger.LogInformation("GetAllProducts action has been called.");
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult GetAllProduct([FromBody] Product product)
+        {
+            _logger.LogWarning("Product has been created!");
+            return Ok(201);
+        }
+
+
+
+    }
+}
